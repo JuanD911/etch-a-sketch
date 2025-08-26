f7b91bb8556db722840f67ea2c44d0f78faf3c1f
JuanD911 <juandaviduarte911@gmail.com>
2025-08-26 13:30:20 -0500

    grid resize function completed

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const grid = document.querySelector(".grid");
 
 function makeGrid(size){
-
-    const cellSize = 640 / size;
-
+    const cellSize = 500 / size;
     for(i = 0; i < size * size; i++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
@@ -11,16 +9,17 @@ function makeGrid(size){
         cell.style.height = `${cellSize}px`;
         grid.appendChild(cell);
     }
-
 }
 
 function clearGrid(){
-
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = "white";
+    });
 }
 
 function paintCell(){
     const cells = document.querySelectorAll(".cell");
-
     cells.forEach((cell) => {
         cell.addEventListener("mouseover", () => {
             cell.style.backgroundColor = "black";
@@ -29,6 +28,42 @@ function paintCell(){
 
 }
 
+function resizeGrid(size){
+    grid.replaceChildren();
+    makeGrid(size);
+    paintCell();
+}
 
-makeGrid(66);
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click",() => {
+    console.log("Grid has been cleared");
+    clearGrid();
+})
+
+const resizeBtn = document.querySelector(".resize")
+const modal = document.querySelector(".modal");
+resizeBtn.addEventListener("click",() => {
+    console.log("Show modal");
+    modal.style.display = "flex";
+})
+
+const applyBtn = document.querySelector("#applySize");
+applyBtn.addEventListener("click", () => {
+    
+    const newSize = document.getElementById("gridSize").value;
+    const sizeInput = document.getElementById("gridSize");
+
+    if(newSize < 8 || newSize > 100){
+        alert("Enter a number between 8 and 100");
+        sizeInput.value = "";
+    }else{
+        console.log("Size applied");
+        resizeGrid(newSize);
+        modal.style.display = "none";
+        sizeInput.value = "";
+    }
+})
+
+
+makeGrid(30);
 paintCell();
